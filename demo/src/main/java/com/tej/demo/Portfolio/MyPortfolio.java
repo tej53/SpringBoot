4b4c6831178f1485869e87edacb668a61e035ba9
tej53 <tej535617@gmail.com>
2026-07-14 11:31:10 +0530

    Add Education and Projects

--- demo/src/main/java/com/tej/demo/Portfolio/MyPortfolio.java
+++ demo/src/main/java/com/tej/demo/Portfolio/MyPortfolio.java
@@ -26,4 +26,55 @@ public class MyPortfolio {
                 "<b>Databases</b> : MySql, PostgresSQL, MongoDB<br>" +
                 "<b>Soft Skills</b> : Problem Solving, Leadership, Team Player<br>" ;
     }
+
+    @GetMapping("/education")
+    public String myEducation(){
+        return "<h1>Education</h1>" +
+
+                "<h2>Graduation</h2>"+
+                "<ul>" +
+                    "<li>Lovely Professional University</li>" +
+                    "<li>Phagwara, Punjab</li>" +
+                    "<li>2023 - 2027</li>" +
+                "</ul><br>" +
+
+                "<h2>Intermediate</h2>" +
+                "<ul>" +
+                    "<li>Narayana Junior College</li>" +
+                    "<li>Tirupati, Andhra Pradesh</li>" +
+                    "<li>2021 - 2023</li>" +
+                "</ul><br>"+
+
+                "<h2>Matriculation</h2>" +
+                "<ul>" +
+                    "<li>ST. Mary's EM High School</li>" +
+                    "<li>Bangarupalyam, Andhra Pradesh</li>" +
+                    "<li>2021<li>" +
+                "</ul>";
+    }
+
+    @GetMapping("/projects")
+    public String myProjects(){
+        return "<h1>Projects</h1>" +
+                "<h2>NexaBot | React, Node.js, Express, FireBase</h2>" +
+                "<ul>" +
+                    "<li>Built an AI-powered coding practice platform with a real-time code editor, integrating the Groq LLM API to deliver\n" +
+                "instant, context-aware feedback on user submissions and simulater</li>" +
+                    "<li>Engineered a Node.js/Express backend with Firebase for secure user authentication, session management,\n" +
+                "and persistent performance tracking across concurrent user</li>" +
+                    "<li>Implemented smart question recommendations and a performance insights dashboard using React with Framer\n" +
+                "Motion animations, enabling personalized learning paths</li>" +
+                "</ul><br>" +
+
+                "<h2>LocalEats | React, Node.js, Express</h2>" +
+                "<ul>" +
+                    "<li>Developed a full-stack campus food ordering platform enabling students to browse multi-vendor menus, search and\n" +
+                "filter items, and place orders through a responsive, mobile-friendly React</li>" +
+                    "<li>Engineered a Node.js/Express backend with MongoDB/Mongoose for scalable multi-vendor menu data\n" +
+                "management, designing RESTful APIs to handle dynamic stall listings and efficient vendor-student order\n" +
+                "interactive</li>" +
+                    "<li>Built an intuitive, mobile-friendly frontend interface using HTML, Tailwind CSS, and JavaScript, significantly\n" +
+                "improving accessibility and users</li>" +
+                "</ul>";
+    }
 }
