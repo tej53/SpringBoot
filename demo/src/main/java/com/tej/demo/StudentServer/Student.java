d7dafd6a547a4086044be22e26929add590a970e
tej53 <tej535617@gmail.com>
2026-07-14 12:56:47 +0530

    POST with student

---
+++ demo/src/main/java/com/tej/demo/StudentServer/Student.java
@@ -0,0 +1,42 @@
+package com.tej.demo.StudentServer;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
--- demo/src/main/java/com/tej/demo/StudentServer/StudentServer.java
+++ demo/src/main/java/com/tej/demo/StudentServer/StudentServer.java
@@ -1,19 +1,20 @@
 package com.tej.demo.StudentServer;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
     //1. Store the Student
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-            id : 1
-            name : Tej
-            Department : CSE
-            age : 25
-            """;
+    public String storeStudent(@RequestBody Student student){
+        int id = student.getId();
+        String name = student.getName();
+        int age = student.getAge();
+        String department = student.getDepartment();
+
+        return "id : " + id + ", name : " + name + ", age : " + age + ", department : " + department ;
     }
 
     //2. Read the Student with id
