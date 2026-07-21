7a4badd122eb915335c8afb9364fad51c2090693
tej53 <tej535617@gmail.com>
2026-07-21 11:02:28 +0530

    Add DTO

---
+++ demo/src/main/java/com/tej/demo/DTO/CreateStudentRequestDTO.java
@@ -0,0 +1,16 @@
+package com.tej.demo.DTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+public class CreateStudentRequestDTO {
+    private String name;
+    private int age;
+    private String department;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ demo/src/main/java/com/tej/demo/DTO/UpdateStudentRequestDTO.java
@@ -0,0 +1,17 @@
+package com.tej.demo.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UpdateStudentRequestDTO {
+
+    private String name;
+    private int age;
+    private String department;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
